a0f3c103491f402ed00c2956de7ef2fb5f9ce52b
Masamba <masamba.randy@gmail.com>
2026-06-16 23:01:12 +0200

    fix: use dj_database_url.config() pour lire DATABASE_URL Railway

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -80,16 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-_DATABASE_URL = os.getenv('DATABASE_URL')
-if _DATABASE_URL:
-    DATABASES = {'default': dj_database_url.parse(_DATABASE_URL)}
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        conn_max_age=600,
+    )
+}
 
 
 # Password validation
